ebcd8d24b44ad31afe25d8655ba6a31c6eb44482
kenezekiel27 <54111815+kenezekiel27@users.noreply.github.com>
2020-04-29 18:20:43 +0000

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'db4free.net',
+	'username' => 'eugenioderick',
+	'password' => 'eugenioderick',
+	'database' => 'carserviceprovid',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
